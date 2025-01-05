7a435cf3cc18c9f3be215f1adac8b41ba647437a
dt3211776496 <57830488+dt3211776496@users.noreply.github.com>
2025-01-05 17:52:53 +0800

    Update powered-by.tsx

--- app/components/powered-by.tsx
+++ app/components/powered-by.tsx
@@ -6,7 +6,7 @@ export function PoweredBy() {
       rel="noreferrer"
       className="5 5 fixed bottom-0 left-0 hidden rounded-tr-md bg-white px-2 py-1 text-xs sm:block"
     >
-    by <span className="font-semibold text-red-700">yips</span>
+      by <span className="font-semibold text-red-700">yips</span>
     </a>
   );
 }
